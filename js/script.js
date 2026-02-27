6910f8840225d9419aa85ecd9f2cee13dd9481aa
Andipunkk <andycarrillo1704@gmail.com>
2026-02-26 23:25:05 -0700

    Se agrego estructura y assets de prueba

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function changeLayer(id, fileName) {
+    const img = document.getElementById(id);
+    img.src = `img/${fileName}`;
+}
+
+function setHairColor(colorName) {
+    // Cambia el pelo y las cejas al mismo tiempo para que combinen
+    changeLayer('layer-pelo', `pelo/estilo1_${colorName}.png`);
+    changeLayer('layer-cejas', `cejas/tipo1_${colorName}.png`);
+}
